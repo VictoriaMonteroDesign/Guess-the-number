b588dad8eb178863ba81f0417cffe1c3ea29184d
M.Victoria Montero <mvictoria.monteroo@gmail.com>
2024-09-17 12:12:06 -0300

    primer commit

---
+++ juego.py
@@ -0,0 +1,38 @@
+import random
+
+def numeroRandom():
+    return(random.randint(1, 100))
+
+numeroRandomGenerado = numeroRandom()
+
+'''
+if numeroRandomGenerado == 15:
+   print("El numero es 15")
+else:
+    print(f"El numero no es 15, es {numeroRandomEjemplo}")
+'''
+
+jugador = input("¡Hola jugador! Por favor ingresa tu nombre\n")
+print(f"Hola {jugador}")
+
+
+print("Voy a elegir un número entre 1 y 100\nAhora adivina.")
+
+for intento in range(4):
+    if intento == 0:
+        juego = int(input("¿Qué número es el que escogí? Tienes 5 intentos\n¡Buena Suerte!\n"))
+    else:
+        juego = int(input("Inténtalo nuevamente\n"))
+
+    if juego == numeroRandomGenerado:
+        print("¡Acertaste!")
+        break
+    elif juego > numeroRandomGenerado:
+        print("Muy alto.")
+    else:
+        print("Muy bajo.")
+
+#intentos = intento[0,1,2,3,4]
+
+if juego != numeroRandomGenerado:
+    print(f"Lo siento, se te acabaron los intentos. El número era {numeroRandomGenerado}")
---
+++ main.py
@@ -0,0 +1,17 @@
+print('hola este es un nuevo documento de python')
+
+'''Para escribir comentarios'''
+
+unString = "Esto es un string"
+print(unString)
+
+def funcionGeneraUnPrint():
+    print("resultado de mi funcion")
+funcionGeneraUnPrint()
+
+print()
+
+import random
+print(random.randint(10, 20))
+
+print()
